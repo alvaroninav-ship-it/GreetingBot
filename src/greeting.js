8d5a41f329e3809f2a60044e5650a71765c46b60
Alvaro Nina <alvaro.nina.v@ubc.edu.bo>
2026-08-20 21:51:43 +0200

    feat: saludar de acuerdo a la hora

--- src/greeting.js
+++ src/greeting.js
@@ -1,5 +1,13 @@
-function Greet(name){
-    return `Hola ${name}, bienvenido, soy un bot que te saluda`;
+function Greet(name) {
+    const hora = new Date().getHours();
+
+    if (hora < 12) {
+        return `Buenos días, ${name}`;
+    } else if (hora < 18) {
+        return `Buenas tardes, ${name}`;
+    } else {
+        return `Buenas noches, ${name}`;
+    }
 }
 
 export default Greet;
